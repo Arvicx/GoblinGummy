202fa63ca94fe46b30ca181c399ceee46c2d41b6
Arthur <arthurfernandesrodrigues1@gmail.com>
2024-11-29 04:36:33 -0300

    Goblin Gummy v3

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,18 @@
 function carregarCatalogo(){
     var produtos = [
-        ["images/Goblin.png","Goblin Teste","R$1000,00"],
-        ["images/GoblinInvertido.png","Goblin Testessss","R$1000,00"]
+        ["images/itens/Goblin Mockup Verde Catalogo.png","Goblin Gummy Sabor Original","R$15,00"],
+        ["images/itens/Goblin Mockup Roxo Catalogo.png","Goblin Gummy Sabor Uva","R$15,00"],
+        ["images/itens/Goblin Mockup Vermelho Catalogo.png","Goblin Gummy Sabor Morango","R$15,00"],
+        ["images/itens/Goblin Mockup Laranja Catalogo.png","Goblin Gummy Sabor Laranja","R$15,00"]
     ]
     produtos.forEach(cadaProduto =>{
         document.getElementById('catalogo').innerHTML += `
         <div class ="produto">
-            <img src="${cadaProduto[0]}" alt="">
-            <h4>${cadaProduto[1]}</h4>
-            <h3>${cadaProduto[2]}</h3>
+            <img id="img-produto" src="${cadaProduto[0]}" alt="">
+            <h4 id="content-produto">${cadaProduto[1]}</h4>
+            <h3 id="content-produto">${cadaProduto[2]}</h3>
 
-            <button>
+            <button id="button-produto">
                 Comprar
                 <img src="images/icons/plus-circle.svg" alt="">
             </button>
@@ -25,10 +27,13 @@ carregarCatalogo()
 
 function carregarInfluencers(){
     var influencers = [
-        ["/images/influencers/GabiCattuzo.jpg", "Gabi Cattuzzo"],
+        ["/images/influencers/Alanzokka", "Alanzoka"],
+        ["/images/influencers/Kai-Cenat.jpg", "Kai Cenat"],
+        ["/images/influencers/GabiCattuzo.jpg", "Gabi Cattuzzo"]
+        
     ]
     influencers.forEach(cadaInfluencer =>{
-        document.querySelector('.carousel-inner').innerHTML += `
+        document.querySelector('#carrossel').innerHTML += `
         <div class="carousel-item">
             <img src="${cadaInfluencer[0]}" class="d-block w-100" alt="...">
             <div class="carousel-caption d-none d-md-block">
