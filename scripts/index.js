476d73f949cd8763aa784507a295da3be49c47d1
Arthur <arthurfernandesrodrigues1@gmail.com>
2024-11-29 22:59:54 -0300

    Goblin Gummy :(

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,15 @@ function carregarCatalogo(){
         ["images/itens/Gummy Verde 10un.png","Goblin Gummy Sabor Original 10x","R$20,90"],
         ["images/itens/Gummy Roxo 10un.png","Goblin Gummy Sabor Uva 10px","R$20,90"],
         ["images/itens/Gummy Vermelho 10un.png","Goblin Gummy Sabor Morango 10px","R$20,90"],
-        ["images/itens/Gummy Laranja 10un.png","Goblin Gummy Sabor Laranja 10px","R$20,90"]
+        ["images/itens/Gummy Laranja 10un.png","Goblin Gummy Sabor Laranja 10px","R$20,90"],
+        ["images/mockups/mockupsCatalogo/Camiseta Cinza Catálogo.png","Camiseta Cinza Goblin!","R$79,90"],
+        ["images/mockups/mockupsCatalogo/Camiseta Vermelha Catálogo.png","Camiseta Vermelha Goblin!","R$79,90"],
+        ["images/mockups/mockupsCatalogo/Moletom Branco Catálogo.png","Moletom Branco Goblin!","R$159,90"],
+        ["images/mockups/mockupsCatalogo/Moletom Branco HotSpace Catálogo.png","Moletom Branco HobGoblin!","R$169,90"],
+        ["images/mockups/mockupsCatalogo/Moletom Preto HotSpace Catálogo.png","Moletom Preto HobGoblin!","R$169,90"],
+        ["images/mockups/mockupsCatalogo/Ecobag Catálogo.png","Ecobag Goblin!","R$25,99"],
+        ["images/mockups/mockupsCatalogo/Caneca Catálogo.png","Caneca Goblin!","R$21,90"]
+
     ]
     produtos.forEach(cadaProduto =>{
         document.getElementById('catalogo').innerHTML += `
@@ -34,7 +42,6 @@ function carregarInfluencers(){
         ["/images/influencers/Alanzokka.jpg", "Alanzoka"],
         ["/images/influencers/Kai-Cenat.jpg", "Kai Cenat"],
         ["/images/influencers/GabiCattuzo.jpg", "Gabi Cattuzzo"]
-        
     ]
     influencers.forEach(cadaInfluencer =>{
         document.querySelector('#carrosselInterno').innerHTML += `
@@ -50,27 +57,3 @@ function carregarInfluencers(){
 }
 
 carregarInfluencers()
-
-function carregarMarcas(){
-    var marcas = [
-        ["/images/marcas/GoldenStateWarriors.png", "Golden State Warriors"],
-        ["/images/marcas/twice.jpg", "Twice"],
-        ["/images/marcas/ValorantChampions.jpg","Valorant Champions Tournament"]
-        ["/images/marcas/PainGaming.png", "Pain Gaming"],
-        ["/images/marcas/OrdemParanormal.jpg","Ordem Paranormal RPG"]
-        
-    ]
-    influencers.forEach(cadaMarca =>{
-        document.querySelector('#carrosselMarca').innerHTML += `
-        <div class="carousel-item">
-            <img src="${cadaMarca[0]}" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>${cadaMarca[1]}</h5>
-            </div>
-        </div>
-        `
-
-    })
-}
-
-carregarMarcas()
